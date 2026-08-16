1269ff63612dc4306949b5504386e897abc3367e
supardi98 <supardi.dev@gmail.com>
2026-08-16 13:26:50 +0700

    refactor: extract applyProCamera2Settings calculations into net.supardi.evcam.ui.ProSettingsUtil.kt

--- app/src/main/kotlin/net/supardi/evcam/CameraScreen.kt
+++ app/src/main/kotlin/net/supardi/evcam/CameraScreen.kt
@@ -765,58 +765,20 @@ fun CameraScreen(modifier: Modifier = Modifier) {
     }
 
     LaunchedEffect(isProMode, iso, shutterSpeed, focusDistance, whiteBalance, manualKelvin, isIsoAuto, isShutterAuto, isFocusAuto, camera2Control) {
-        camera2Control?.let { control ->
-            val builder = androidx.camera.camera2.interop.CaptureRequestOptions.Builder()
-            if (isProMode) {
-                if (isIsoAuto && isShutterAuto) {
-                    builder.clearCaptureRequestOption(android.hardware.camera2.CaptureRequest.CONTROL_AE_MODE)
-                } else {
-                    builder.setCaptureRequestOption(android.hardware.camera2.CaptureRequest.CONTROL_AE_MODE, android.hardware.camera2.CaptureRequest.CONTROL_AE_MODE_OFF)
-                    builder.setCaptureRequestOption(android.hardware.camera2.CaptureRequest.SENSOR_SENSITIVITY, iso.toInt())
-                    builder.setCaptureRequestOption(android.hardware.camera2.CaptureRequest.SENSOR_EXPOSURE_TIME, shutterSpeed.toLong())
-                }
-                
-                if (isFocusAuto) {
-                    builder.clearCaptureRequestOption(android.hardware.camera2.CaptureRequest.CONTROL_AF_MODE)
-                } else {
-                    builder.setCaptureRequestOption(android.hardware.camera2.CaptureRequest.CONTROL_AF_MODE, android.hardware.camera2.CaptureRequest.CONTROL_AF_MODE_OFF)
-                    builder.setCaptureRequestOption(android.hardware.camera2.CaptureRequest.LENS_FOCUS_DISTANCE, focusDistance)
-                }
-                
-                builder.setCaptureRequestOption(android.hardware.camera2.CaptureRequest.CONTROL_AWB_MODE, whiteBalance)
-                if (whiteBalance == android.hardware.camera2.CaptureRequest.CONTROL_AWB_MODE_OFF) {
-                    builder.setCaptureRequestOption(android.hardware.camera2.CaptureRequest.COLOR_CORRECTION_MODE, android.hardware.camera2.CaptureRequest.COLOR_CORRECTION_MODE_TRANSFORM_MATRIX)
-                    val temp = manualKelvin / 100.0f
-                    var r: Float
-                    var g: Float
-                    var b: Float
-                    if (temp <= 66.0f) {
-                        r = 255.0f
-                        g = (99.4708025861f * Math.log(temp.toDouble()).toFloat() - 161.1195681661f).coerceIn(0f, 255f)
-                        b = if (temp <= 19.0f) 0.0f else (138.5177312231f * Math.log(temp.toDouble() - 10.0).toFloat() - 305.0447927307f).coerceIn(0f, 255f)
-                    } else {
-                        r = (329.698727446f * Math.pow(temp.toDouble() - 60.0, -0.1332047592).toFloat()).coerceIn(0f, 255f)
-                        g = (288.1221695283f * Math.pow(temp.toDouble() - 60.0, -0.0755148492).toFloat()).coerceIn(0f, 255f)
-                        b = 255.0f
-                    }
-                    val rGain = (255f / r).coerceIn(1f, 3.5f)
-                    val gGain = (255f / g).coerceIn(1f, 3.5f)
-                    val bGain = (255f / b).coerceIn(1f, 3.5f)
-                    builder.setCaptureRequestOption(android.hardware.camera2.CaptureRequest.COLOR_CORRECTION_GAINS, android.hardware.camera2.params.RggbChannelVector(rGain, gGain, gGain, bGain))
-                } else {
-                    builder.clearCaptureRequestOption(android.hardware.camera2.CaptureRequest.COLOR_CORRECTION_MODE)
-                    builder.clearCaptureRequestOption(android.hardware.camera2.CaptureRequest.COLOR_CORRECTION_GAINS)
-                }
-            } else {
-                builder.clearCaptureRequestOption(android.hardware.camera2.CaptureRequest.CONTROL_AE_MODE)
-                builder.clearCaptureRequestOption(android.hardware.camera2.CaptureRequest.CONTROL_AF_MODE)
-                builder.clearCaptureRequestOption(android.hardware.camera2.CaptureRequest.CONTROL_AWB_MODE)
-                builder.clearCaptureRequestOption(android.hardware.camera2.CaptureRequest.COLOR_CORRECTION_MODE)
-                builder.clearCaptureRequestOption(android.hardware.camera2.CaptureRequest.COLOR_CORRECTION_GAINS)
-            }
-            control.captureRequestOptions = builder.build()
-        }
+        applyProCamera2Settings(
+            camera2Control = camera2Control,
+            isProMode = isProMode,
+            isIsoAuto = isIsoAuto,
+            isShutterAuto = isShutterAuto,
+            isFocusAuto = isFocusAuto,
+            iso = iso,
+            shutterSpeed = shutterSpeed,
+            focusDistance = focusDistance,
+            whiteBalance = whiteBalance,
+            manualKelvin = manualKelvin
+        )
     }
+
 
     Box(modifier = modifier
         .fillMaxSize()
---
+++ app/src/main/kotlin/net/supardi/evcam/ui/ProSettingsUtil.kt
@@ -0,0 +1,71 @@
+package net.supardi.evcam.ui
+
+import androidx.camera.camera2.interop.Camera2CameraControl
+import androidx.camera.camera2.interop.CaptureRequestOptions
+import android.hardware.camera2.CaptureRequest
+import android.hardware.camera2.params.RggbChannelVector
+
+fun applyProCamera2Settings(
+    camera2Control: Camera2CameraControl?,
+    isProMode: Boolean,
+    isIsoAuto: Boolean,
+    isShutterAuto: Boolean,
+    isFocusAuto: Boolean,
+    iso: Float,
+    shutterSpeed: Float,
+    focusDistance: Float,
+    whiteBalance: Int,
+    manualKelvin: Float
+) {
+    camera2Control?.let { control ->
+        val builder = CaptureRequestOptions.Builder()
+        if (isProMode) {
+            if (isIsoAuto && isShutterAuto) {
+                builder.clearCaptureRequestOption(CaptureRequest.CONTROL_AE_MODE)
+            } else {
+                builder.setCaptureRequestOption(CaptureRequest.CONTROL_AE_MODE, CaptureRequest.CONTROL_AE_MODE_OFF)
+                builder.setCaptureRequestOption(CaptureRequest.SENSOR_SENSITIVITY, iso.toInt())
+                builder.setCaptureRequestOption(CaptureRequest.SENSOR_EXPOSURE_TIME, shutterSpeed.toLong())
+            }
+            
+            if (isFocusAuto) {
+                builder.clearCaptureRequestOption(CaptureRequest.CONTROL_AF_MODE)
+            } else {
+                builder.setCaptureRequestOption(CaptureRequest.CONTROL_AF_MODE, CaptureRequest.CONTROL_AF_MODE_OFF)
+                builder.setCaptureRequestOption(CaptureRequest.LENS_FOCUS_DISTANCE, focusDistance)
+            }
+            
+            builder.setCaptureRequestOption(CaptureRequest.CONTROL_AWB_MODE, whiteBalance)
+            if (whiteBalance == CaptureRequest.CONTROL_AWB_MODE_OFF) {
+                builder.setCaptureRequestOption(CaptureRequest.COLOR_CORRECTION_MODE, CaptureRequest.COLOR_CORRECTION_MODE_TRANSFORM_MATRIX)
+                val temp = manualKelvin / 100.0f
+                var r: Float
+                var g: Float
+                var b: Float
+                if (temp <= 66.0f) {
+                    r = 255.0f
+                    g = (99.4708025861f * Math.log(temp.toDouble()).toFloat() - 161.1195681661f).coerceIn(0f, 255f)
+                    b = if (temp <= 19.0f) 0.0f else (138.5177312231f * Math.log(temp.toDouble() - 10.0).toFloat() - 305.0447927307f).coerceIn(0f, 255f)
+                } else {
+                    r = (329.698727446f * Math.pow(temp.toDouble() - 60.0, -0.1332047592).toFloat()).coerceIn(0f, 255f)
+                    g = (288.1221695283f * Math.pow(temp.toDouble() - 60.0, -0.0755148492).toFloat()).coerceIn(0f, 255f)
+                    b = 255.0f
+                }
+                val rGain = (255f / r).coerceIn(1f, 3.5f)
+                val gGain = (255f / g).coerceIn(1f, 3.5f)
+                val bGain = (255f / b).coerceIn(1f, 3.5f)
+                builder.setCaptureRequestOption(CaptureRequest.COLOR_CORRECTION_GAINS, RggbChannelVector(rGain, gGain, gGain, bGain))
+            } else {
+                builder.clearCaptureRequestOption(CaptureRequest.COLOR_CORRECTION_MODE)
+                builder.clearCaptureRequestOption(CaptureRequest.COLOR_CORRECTION_GAINS)
+            }
+        } else {
+            builder.clearCaptureRequestOption(CaptureRequest.CONTROL_AE_MODE)
+            builder.clearCaptureRequestOption(CaptureRequest.CONTROL_AF_MODE)
+            builder.clearCaptureRequestOption(CaptureRequest.CONTROL_AWB_MODE)
+            builder.clearCaptureRequestOption(CaptureRequest.COLOR_CORRECTION_MODE)
+            builder.clearCaptureRequestOption(CaptureRequest.COLOR_CORRECTION_GAINS)
+        }
+        control.captureRequestOptions = builder.build()
+    }
+}
