438f6686351ce5abab3a9ff26a837ac76ddc20dc
supardi98 <supardi.dev@gmail.com>
2026-08-19 13:22:08 +0700

    chore: bump version to 0.1.0 for release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "net.supardi.evcam"
         minSdk = 24
         targetSdk = 34
-        versionCode = 1
-        versionName = "0.0.1"
+        versionCode = 2
+        versionName = "0.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
